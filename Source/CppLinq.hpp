77a9b3b88e657be32a1176acc822297fd14795ca
Pavel Dubsky <pavel.dubsky@padutronics.com>
2022-09-02 15:40:07 +0300

    Rename template array length parameter.

--- Source/CppLinq.hpp
+++ Source/CppLinq.hpp
@@ -11,9 +11,9 @@ namespace CppLinq
         return { { container.cbegin(), container.cend() } };
     }
 
-    template <typename T, std::uint32_t SIZE>
-    auto From(const T (&array)[SIZE]) -> Details::Query<Details::Enumerators::IteratorEnumerator<const T*>>
+    template <typename T, std::uint32_t TLength>
+    auto From(const T (&array)[TLength]) -> Details::Query<Details::Enumerators::IteratorEnumerator<const T*>>
     {
-        return { { reinterpret_cast<const T*>(&array), reinterpret_cast<const T*>(&array) + SIZE } };
+        return { { reinterpret_cast<const T*>(&array), reinterpret_cast<const T*>(&array) + TLength } };
     }
 }
