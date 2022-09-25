50adbf00e7f9e4d9d0a964b360219931a9de570b
Pavel Dubsky <pavel.dubsky@padutronics.com>
2022-09-25 22:05:03 +0300

    Update tests for 'Average' query.

---
+++ Tests/Average/Average.cpp
@@ -0,0 +1,78 @@
+#include <vector>
+
+#include "Assertions.hpp"
+#include "CppLinq.hpp"
+
+using namespace CppLinq::Exceptions;
+using namespace std;
+
+namespace CppLinq::Tests::Average
+{
+    TEST(Average, Throws_if_source_is_empty)
+    {
+        // Arrange.
+        const vector<double> source;
+
+        const auto query = From(source);
+
+        // Act.
+        const auto action = [&query](){ query.Average(); };
+
+        // Assert.
+        EXPECT_THROW(action(), EmptyContainerException);
+    }
+
+    TEST(Average, Returns_average_value_if_source_contains_one_element)
+    {
+        // Arrange.
+        const double source[]{ 1.0 };
+        const auto expected = 1.0;
+
+        // Act.
+        const auto actual = From(source).Average();
+
+        // Assert.
+        EXPECT_EQ(actual, expected);
+    }
+
+    TEST(Average, Returns_average_value_if_source_contains_two_elements)
+    {
+        // Arrange.
+        const double source[]{ 1.0, 2.0 };
+        const auto expected = 1.5;
+
+        // Act.
+        const auto actual = From(source).Average();
+
+        // Assert.
+        EXPECT_EQ(actual, expected);
+    }
+
+    TEST(Average, Returns_average_value_if_source_contains_three_elements)
+    {
+        // Arrange.
+        const double source[]{ 1.0, 2.0, 3.0 };
+        const auto expected = 2.0;
+
+        // Act.
+        const auto actual = From(source).Average();
+
+        // Assert.
+        EXPECT_EQ(actual, expected);
+    }
+
+    TEST(Average, Returns_the_same_results)
+    {
+        // Arrange.
+        const double source[]{ 1.0, 2.0, 3.0, 4.0, 5.0 };
+
+        const auto query = From(source);
+
+        // Act.
+        const auto actual1 = query.Average();
+        const auto actual2 = query.Average();
+
+        // Assert.
+        EXPECT_EQ(actual1, actual2);
+    }
+}
