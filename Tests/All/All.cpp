c9d7b9bc1ecbd46adb2769f781b38091c19c875e
Pavel Dubsky <pavel.dubsky@padutronics.com>
2022-09-25 21:48:26 +0300

    Update tests for 'All' query.

---
+++ Tests/All/All.cpp
@@ -0,0 +1,73 @@
+#include <vector>
+
+#include "Assertions.hpp"
+#include "CppLinq.hpp"
+
+using namespace CppLinq::Exceptions;
+using namespace std;
+
+namespace CppLinq::Tests::All
+{
+    TEST(All, Returns_true_if_source_is_empty)
+    {
+        // Arrange.
+        const vector<int> source;
+
+        // Act.
+        const auto actual = From(source).All([](const int value){ return value % 2 == 0; });
+
+        // Assert.
+        EXPECT_TRUE(actual);
+    }
+
+    TEST(All, Returns_true_if_source_contains_only_valid_elements)
+    {
+        // Arrange.
+        const int source[]{ 2, 4, 6, 8, 10 };
+
+        // Act.
+        const auto actual = From(source).All([](const int value){ return value % 2 == 0; });
+
+        // Assert.
+        EXPECT_TRUE(actual);
+    }
+
+    TEST(All, Returns_false_if_source_contains_only_invalid_elements)
+    {
+        // Arrange.
+        const int source[]{ 1, 3, 5, 7, 9 };
+
+        // Act.
+        const auto actual = From(source).All([](const int value){ return value % 2 == 0; });
+
+        // Assert.
+        EXPECT_FALSE(actual);
+    }
+
+    TEST(All, Returns_false_if_source_contains_valid_and_invalid_elements)
+    {
+        // Arrange.
+        const int source[]{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        // Act.
+        const auto actual = From(source).All([](const int value){ return value % 2 == 0; });
+
+        // Assert.
+        EXPECT_FALSE(actual);
+    }
+
+    TEST(All, Returns_the_same_results)
+    {
+        // Arrange.
+        const int source[]{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        const auto query = From(source);
+
+        // Act.
+        const auto actual1 = query.All([](const int value){ return value % 2 == 0; });
+        const auto actual2 = query.All([](const int value){ return value % 2 == 0; });
+
+        // Assert.
+        EXPECT_EQ(actual1, actual2);
+    }
+}
