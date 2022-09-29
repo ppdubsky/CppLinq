6a2cf9b0b97b9e315c8f0072aaedb8919a7f50ab
Pavel Dubsky <pavel.dubsky@padutronics.com>
2022-09-30 00:56:18 +0300

    Update tests for 'Sum' query.

--- Tests/Sum/Compute_the_sum.cpp
+++ Tests/Sum/Compute_the_sum.cpp
@@ -3,12 +3,11 @@
 #include "Assertions.hpp"
 #include "CppLinq.hpp"
 
-using namespace CppLinq::Exceptions;
 using namespace std;
 
 namespace CppLinq::Tests::Sum
 {
-    TEST(Sum, ReturnsSum_SourceIsEmpty)
+    TEST(Compute_the_sum, Returns_zero_if_source_is_empty)
     {
         // Arrange.
         const vector<int> source;
@@ -21,7 +20,7 @@ namespace CppLinq::Tests::Sum
         EXPECT_EQ(actual, expected);
     }
 
-    TEST(Sum, ReturnsSum_SourceIsNotEmpty)
+    TEST(Compute_the_sum, Returns_sum_if_source_is_not_empty)
     {
         // Arrange.
         const int source[]{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
@@ -34,7 +33,7 @@ namespace CppLinq::Tests::Sum
         EXPECT_EQ(actual, expected);
     }
 
-    TEST(Sum, ReturnsSameResults)
+    TEST(Compute_the_sum, Returns_the_same_result_on_every_call)
     {
         // Arrange.
         const int source[]{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
