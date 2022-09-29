131e02a690e10597400a064f19113f43948f3918
Pavel Dubsky <pavel.dubsky@padutronics.com>
2022-09-29 12:08:11 +0300

    Update tests for 'Prepend' query.

---
+++ Tests/Prepend/Prepend_element.cpp
@@ -0,0 +1,76 @@
+#include <vector>
+
+#include "Assertions.hpp"
+#include "CppLinq.hpp"
+
+using namespace std;
+
+namespace CppLinq::Tests::Prepend
+{
+    TEST(Prepend, Returns_expected_sequence_if_source_is_empty)
+    {
+        // Arrange.
+        const vector<int> source;
+        const int expected[]{ 7 };
+
+        // Act.
+        const auto actual = From(source).Prepend(7);
+
+        // Assert.
+        ExpectSequencesAreEquivalent(actual, expected);
+    }
+
+    TEST(Prepend, Returns_expected_sequence_if_source_contains_one_element)
+    {
+        // Arrange.
+        const int source[]{ 1 };
+        const int expected[]{ 7, 1 };
+
+        // Act.
+        const auto actual = From(source).Prepend(7);
+
+        // Assert.
+        ExpectSequencesAreEquivalent(actual, expected);
+    }
+
+    TEST(Prepend, Returns_expected_sequence_if_source_contains_two_elements)
+    {
+        // Arrange.
+        const int source[]{ 1, 2 };
+        const int expected[]{ 7, 1, 2 };
+
+        // Act.
+        const auto actual = From(source).Prepend(7);
+
+        // Assert.
+        ExpectSequencesAreEquivalent(actual, expected);
+    }
+
+    TEST(Prepend, Returns_expected_sequence_if_source_contains_three_elements)
+    {
+        // Arrange.
+        const int source[]{ 1, 2, 3 };
+        const int expected[]{ 7, 1, 2, 3 };
+
+        // Act.
+        const auto actual = From(source).Prepend(7);
+
+        // Assert.
+        ExpectSequencesAreEquivalent(actual, expected);
+    }
+
+    TEST(Prepend, Returns_the_same_result_on_every_call)
+    {
+        // Arrange.
+        const int source[]{ 1, 2, 3, 4, 5 };
+
+        const auto query = From(source);
+
+        // Act.
+        const auto actual1 = query.Prepend(7);
+        const auto actual2 = query.Prepend(7);
+
+        // Assert.
+        ExpectSequencesAreEquivalent(actual1, actual2);
+    }
+}
